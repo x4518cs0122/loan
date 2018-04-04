5c996c720355104225be5bcfd349755c07df9925
LTY <360436029@qq.com>
2018-04-04 09:40:59 +0800

    little change

--- src/components/section-one/evaluate/unreported.vue
+++ src/components/section-one/evaluate/unreported.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="unreported">
-        <v-header :title="title"></v-header>
+        <v-header :title="title" @submit="submit" :next="next"></v-header>
         <ul>
             <li v-for="(type,index) in reports" :key="index">
                 <h2 class="title" v-if="type.title">{{type.title}}</h2>
                 <ul>
                     <li v-for="(item,index) in type.items" :key="index">
-                        <div v-if="item.class==='select-wrapper'" :class="item.class">
+                        <div v-if="item.class==='select-wrapper'" :class="item.class" @click="pop(item)">
                             <span class="text">{{item.text}}</span>
-                            <span class="desc">{{item.val}}</span>
+                            <span class="desc">{{item.value}}</span>
                             <span class="icon">
                                 <span class="fa fa-chevron-right" aria-hidden="true"></span>
                             </span>
@@ -24,31 +24,34 @@
                 </ul>
             </li>
         </ul>
+        <pop :options="options" ref="pop" @choosed="choosed"></pop>
     </div>
 </template>
 
 <script>
 import vHeader from 'base/header/header'
+import pop from 'base/pop-up/pop-up'
  export default {
      data(){
          return{
              title:'出报告',
+             next:'提交',
              reports:[
                  {
                      title:'',
                      items:[{
                          class:'select-wrapper',
                          text:'环节状态',
-                         val:'',
+                         value:'',
                          options:['已出报告','未出报告,继续提醒我']
                      },{
                          class:'select-wrapper',
                          text:'完成日期',
-                         val:'选择出报告日期'
+                         value:'选择出报告日期'
                      },{
                          class:'select-wrapper',
                          text:'报告类型',
-                         val:'',
+                         value:'',
                          options:['预评','正评']
                      }]
                  },{
@@ -105,11 +108,35 @@ import vHeader from 'base/header/header'
                         options:['首套','两套']
                     }]
                  }
-                ]
+                ],
+             options:[],
+             currentItem:{}
+         }
+     },
+     methods:{
+         pop(item){
+             this.options = item.options
+             this.currentItem = item
+             this.$refs.pop.show()
+         },
+         choosed(option){
+             this.currentItem.value = option
+         },
+         submit(){
+             this.normalize()
+         },
+         normalize(){
+             let result = this.reports.map((item)=>{
+                 return item.items.map((i)=>{
+                     return i.value
+                 })
+             })
+             console.log(result)
          }
      },
      components:{
-         vHeader
+         vHeader,
+         pop
      }
  }
 </script>
